b253629c2cf6edfe108bf04ecc70e3282ba3a476
gness1804 <graham.nessler@gmail.com>
2020-12-04 17:07:14 -0600

    Hook up App to use redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,21 @@
 import React, { ReactNode } from 'react';
+import { connect } from 'react-redux';
+import { fetchTodos } from '../actions';
+import { Todo, StoreState } from '../types';
 
-export class App extends React.Component {
+interface Props {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends React.Component<Props> {
   render(): ReactNode {
     return <div>Hello</div>;
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => ({
+  todos,
+});
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
