6668b230373e4532ef7b3b9d68af695b03eecb94
gness1804 <graham.nessler@gmail.com>
2020-12-04 13:47:41 -0600

    Take two for fixing CRA eslint config problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
-module.exports = {
+// thanks to jrr in https://github.com/facebook/create-react-app/issues/9929 for this solution. It's his "terrible, no good, really bad workaround" to the problem of Create React App breaking compile for eslint errors
+const activeConfig = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: [
@@ -12,5 +13,12 @@ module.exports = {
     '@typescript-eslint/ban-types': 'warn',
     '@typescript-eslint/no-unused-vars': 'error',
     'no-console': 'warn',
+    'no-undef': 'error'
   },
 };
+
+const inactiveConfig = {
+  ignorePatterns: ["**/*.ts", "**/*.tsx", "./*.js", "config/*.js"],
+}
+
+module.exports = process.env.DISABLE_ES_LINT === 'true' ? inactiveConfig : activeConfig;
