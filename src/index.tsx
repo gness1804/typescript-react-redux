39862453cb7fd03af12615a4e6e07206d0130b7b
gness1804 <graham.nessler@gmail.com>
2020-12-04 11:20:08 -0600

    Add prop interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+interface Props {
+  name: string;
+}
+
+class App extends React.Component<Props> {
   render() {
-    return <div>Hello there</div>;
+    return <div>Hello there, {this.props.name}</div>;
   }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App name="Graham" />, document.querySelector('#root'));
