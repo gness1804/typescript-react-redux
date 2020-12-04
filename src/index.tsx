5976e037dc0aa29e1efd39ba071769188890a67a
gness1804 <graham.nessler@gmail.com>
2020-12-04 12:03:42 -0600

    Add state interface

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,18 @@ interface Props {
   name: string;
 }
 
-class App extends React.Component<Props> {
-  state = {
-    counter: 0,
-  };
+interface State {
+  counter: number;
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
 
   increaseCount = (): void => {
     this.setState({ counter: this.state.counter + 1 });
